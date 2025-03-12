9ce810c660816bc9ccbb1f399635ba1b2a392b39
mayudev <99561108+mayudev@users.noreply.github.com>
2025-03-12 22:06:35 +0100

    chore: revert vercel stuff...

--- packages/server/dist/lib/date.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseStationWhen = void 0;
-const parseStationWhen = (queryData) => queryData.when ? { when: new Date(queryData.when) } : {};
-exports.parseStationWhen = parseStationWhen;
