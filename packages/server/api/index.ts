a3eadfb702c5f4105957f94097d671e2ab4d90c2
mayudev <99561108+mayudev@users.noreply.github.com>
2025-03-12 22:53:28 +0100

    chore: remove listen for serverless

--- packages/server/api/index.ts
+++ packages/server/api/index.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use('/api', api);
 
 const port = process?.env.APP_PORT || 8080;
-app.listen(port);
+//app.listen(port);
 console.log(`Running on port ${port}`);
 
 export default app;
