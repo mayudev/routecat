503065149038c54e28cb816663b30e91b4a96fe1
mayudev <99561108+mayudev@users.noreply.github.com>
2025-03-15 21:31:12 +0100

    feat(web): add error message

--- packages/web/src/pages/Journey.tsx
+++ packages/web/src/pages/Journey.tsx
@@ -5,11 +5,12 @@ import { Spinner } from '../components/atoms/Spinner';
 import { JourneyCard } from '../components/organisms/JourneyCard';
 
 import styles from './Journey.module.css';
+import { PiXCircle } from 'react-icons/pi';
 
 export const Journey = () => {
   const [searchParams] = useSearchParams();
 
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ['journey', searchParams.toString()],
     queryFn: journeyQueryFn({
       origin: searchParams.get(JourneyParams.origin)!,
@@ -25,6 +26,11 @@ export const Journey = () => {
         <div className={styles.spinner}>
           <Spinner />
           Looking for the best routes for you
+        </div>
+      ) : isError ? (
+        <div className={styles.spinner}>
+          <PiXCircle size={48} />
+          Sorry, an error has occurred.
         </div>
       ) : (
         <div>
